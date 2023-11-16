2b054bb4ee73251c528b854de425e63d3ac97396
julio cesar <jcmcf2012@gmail.com>
2023-11-16 10:52:21 -0300

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solid({ adapter: vercel() })],
+});
